fecf2641a73de222f32f5e264d58112cf31d2c84
Fabian von Feilitzsch <fabian@fabianism.us>
2017-03-01 16:32:21 -0500

    add playbook, update demo.sh for permissions + playbook

--- demo.sh
+++ demo.sh
@@ -1,11 +1,11 @@
 set -x
 oc login --insecure-skip-tls-verify=true `sudo $(which minishift) ip`:8443 -u admin -p admin
-oc new-project wp-demo-$RANDOM
-oc create -f volumes.yml
-oc get pv | awk '{print $1 " " $2 " " $5 " " $6}'  | column -t
-oc get pvc | awk '{print $1" " $2" " $3}' | column -t
-oc create -f etcd.yml
-oc get pods
-oc create -f mysql.yml
-oc get pods
-oc create -f wordpress.yml
+permissions_set=`oc get clusterrole system:hpa-controller -o yaml | grep replicasets`
+if [[ -z "$permissions_set" ]]
+then
+  {
+    oc get clusterrole system:hpa-controller -o yaml ;
+    echo '- apiGroups:\r  - extensions\r  - ""\r  attributeRestrictions: null\r  resources:\r  - replicasets/scale\r  verbs:\r  - get\r  - update';
+  }  | oc replace -f -
+fi
+ansible-playbook provision.yaml -e "project_name=wordpress-ha-demo-$RANDOM"
