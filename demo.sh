a955c8be37b1cb52394201309404f121bb410f41
Fabian von Feilitzsch <fabian@fabianism.us>
2017-03-02 14:18:07 -0500

    fix permissions line

--- demo.sh
+++ demo.sh
@@ -3,9 +3,6 @@ oc login --insecure-skip-tls-verify=true `sudo $(which minishift) ip`:8443 -u ad
 permissions_set=`oc get clusterrole system:hpa-controller -o yaml | grep replicasets`
 if [[ -z "$permissions_set" ]]
 then
-  {
-    oc get clusterrole system:hpa-controller -o yaml ;
-    echo '- apiGroups:\r  - extensions\r  - ""\r  attributeRestrictions: null\r  resources:\r  - replicasets/scale\r  verbs:\r  - get\r  - update';
-  }  | oc replace -f -
+  { oc get clusterrole system:hpa-controller -o yaml ; echo '- {apiGroups: [extensions], attributeRestrictions: null,  resources: [replicasets/scale], verbs: [get,update]}'; }  | oc replace -f -
 fi
 ansible-playbook provision.yaml -e "project_name=wordpress-ha-demo-$RANDOM"
